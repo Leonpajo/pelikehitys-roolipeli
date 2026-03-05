ec5b04d208fb14f237f18cd4ad55443c3e2cf87c
Leonpajo <leonpajo123@gmail.com>
2026-03-05 11:35:34 +0200

    muutokset
    gamemanager, playerdatamanager

---
+++ pelikehitysroolipeli/Assets/Scripts/PlayerDataManager.cs
@@ -0,0 +1,30 @@
+using Unity.Properties;
+using UnityEngine;
+
+public class PlayerDataManager : MonoBehaviour
+{
+    int RemoveHealth;
+    int AddMoney;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void AddExperience()
+    {
+
+    }
+    private void AddHealth() 
+    { 
+
+    }
+}
